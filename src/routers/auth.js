7ea06f5fd19fde4b5904ad15a8d805f6df62215f
ValeriyaHr <152651276+ValeriyaHr@users.noreply.github.com>
2024-10-16 10:54:36 +0300

    add authenticate

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -1,5 +1,11 @@
 import { Router } from "express";
+import { authenticate } from '../middlewares/authenticate.js';
+
+authRouter.get('/data', authenticate, ctrlWrapper(getUserCurrentUserController));
+authRouter.patch('/userId', jsonParser, authenticate, validateBody(updateDataUserSchema), ctrlWrapper(updateDataUserController));
+authRouter.patch('/avatar', authenticate, upload.single('avatar'), ctrlWrapper(updateDataUserController));
 
 const authRouter = Router();
+
 
 export default authRouter;
--- src/routers/water.js
+++ src/routers/water.js
@@ -1,4 +1,7 @@
 import { Router } from "express";
+import { authenticate } from '../middlewares/authenticate.js';
+
+router.use(authenticate);
 
 const waterRouter = Router();
 
