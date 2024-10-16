e63edc39cc072a922720a22855b92c1506a1be48
ValeriyaHr <152651276+ValeriyaHr@users.noreply.github.com>
2024-10-16 12:39:15 +0300

    update auth.js

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,10 +1,6 @@
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
-import { randomBytes } from 'crypto';
-import createHttpError from 'http-errors';
+
 import { UsersCollection } from '../db/models/User.js';
 import { SessionCollection } from '../db/models/Session.js';
-import { accessTokenLifeTime, refreshTokenLifeTime } from '../constants/users.js';
 
 
 export const findSessionByAccessToken = async (accessToken) => {
@@ -15,5 +11,3 @@ export const findSessionByAccessToken = async (accessToken) => {
 export const findUser = async (filter) => {
   return await UsersCollection.findOne(filter);
 };
-
-
