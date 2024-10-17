7cda99e3b5468aa73c7b6e5dd7c5002c4f0ad810
Mark-goga <markgala06@gmail.com>
2024-10-17 15:48:01 +0300

    add get water for a month

--- src/routers/water.js
+++ src/routers/water.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { authenticate } from '../middlewares/authenticate.js';
+import ctrlWrapper from "../utils/ctrlWrapper.js";
+import { getWaterForMonthController } from "../controllers/water.js";
 
 const waterRouter = Router();
 
 waterRouter.use(authenticate);
-
+waterRouter.use("/month" , ctrlWrapper(getWaterForMonthController));
 
 export default waterRouter;
