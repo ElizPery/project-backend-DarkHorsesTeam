c5f40592d6e7a9a7bb43cb41489ee954b68f0da3
Mark-goga <markgala06@gmail.com>
2024-10-17 17:01:31 +0300

    took await from services

--- src/controllers/water.js
+++ src/controllers/water.js
@@ -3,7 +3,7 @@ import { getWaterForMonth } from "../services/water.js";
 
 export async function getWaterForMonthController(req , res) {
   const {month} = req.body;
-  const userId = await req.user._id;
+  const userId = req.user._id;
   const year = new Date().getFullYear();
 
   const data = await getWaterForMonth({month , userId , year});
